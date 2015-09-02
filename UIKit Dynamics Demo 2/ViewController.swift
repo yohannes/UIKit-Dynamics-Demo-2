e36b0302007a421c3cfacee068c4daec1ae5af66
Yohannes Wijaya <yohannes.wijaya@gmail.com>
2015-09-02 11:44:11 +0700

    Setup the box, collision and animation behaviors.

--- UIKit Dynamics Demo 2/ViewController.swift
+++ UIKit Dynamics Demo 2/ViewController.swift
@@ -7,12 +7,35 @@
 //
 
 import UIKit
+import QuartzCore
 
 class ViewController: UIViewController {
+    
+    // MARK: - Stored Properties
+    
+    let greenBoxSize = CGSizeMake(100.0, 100.0)
+    var greenBoxView: UIView!
+    var dynamicAnimator: UIDynamicAnimator!
+    var collisionBehavior: UICollisionBehavior!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // MARK: Box
+        self.greenBoxView = UIView(frame: CGRectMake(CGRectGetMidX(self.view.bounds) - (self.greenBoxSize.width / 2), CGRectGetMidY(self.view.bounds) - (self.greenBoxSize.height / 2), self.greenBoxSize.width, self.greenBoxSize.height))
+        self.greenBoxView.backgroundColor = UIColor.greenColor()
+        self.greenBoxView.transform = CGAffineTransformMakeRotation(CGFloat(M_PI / 8))
+        
+        self.view.addSubview(self.greenBoxView)
+        
+        // MARK: Animation
+        self.dynamicAnimator = UIDynamicAnimator(referenceView: self.view)
+        
+        // MARK: Collision
+        self.collisionBehavior = UICollisionBehavior(items: [self.greenBoxView])
+        self.collisionBehavior.translatesReferenceBoundsIntoBoundary = true
+        
+        self.dynamicAnimator.addBehavior(self.collisionBehavior)
     }
 
     override func didReceiveMemoryWarning() {
